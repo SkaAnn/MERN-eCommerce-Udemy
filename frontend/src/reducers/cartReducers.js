9ec186fc5129a15c1fc10ab8146b909720a91e9b
SkaAnn <xskachova@stuba.sk>
2021-02-10 11:02:05 +0100

    Cart reducer and action

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,30 @@
+// only one reducer...
+
+import { CART_ADD_ITEM } from '../constants/cartConstants'
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+    // cartItems - mozme mat viac poloziek v kosiku
+    switch (action.type) {
+        case CART_ADD_ITEM:
+            const item = action.payload
+
+            const existItem = state.cartItems.find(x => x.product === item.product)
+
+            if (existItem) {
+                return {
+                    ...state,
+                    cartItems: state.cartItems.map(x => x.product === existItem.product
+                        ? item : x)
+                }
+            } else {
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, item]
+                }
+            }
+
+        default:
+            return state
+    }
+
+}
