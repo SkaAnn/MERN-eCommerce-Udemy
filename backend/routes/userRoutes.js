0fcde94a79080e7b7dda54ff78c82686389d5ba2
SkaAnn <xskachova@stuba.sk>
2021-02-11 12:46:08 +0100

    Register user backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, getUserProfile } from '../controllers/userControllers.js'
+import { authUser, registerUser, getUserProfile } from '../controllers/userControllers.js'
 import { protect } from '../middleware/authMiddleware.js'
 
-// /api/products je tam kde to volame
+// /api/users je tam kde to volame
+router.route('/').post(registerUser)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)   // use middleware as first argument
 
