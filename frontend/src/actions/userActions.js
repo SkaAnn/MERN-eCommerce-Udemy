b43607ad7c966a0a155978bbb5edd9a803eb3aba
SkaAnn <xskachova@stuba.sk>
2021-02-12 10:52:30 +0100

    Login and logout user

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants"
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from "../constants/userConstants"
 import axios from 'axios'
 
 export const login = (email, password) => async (dispatch) => {
@@ -32,4 +32,13 @@ export const login = (email, password) => async (dispatch) => {
                 ? error.response.data.message : error.message,
         })
     }
+}
+
+// Logout action
+export const logout = () => (dispatch) => {
+    // remove from local storage
+    localStorage.removeItem('userInfo')
+    dispatch({
+        type: USER_LOGOUT
+    })
 }
