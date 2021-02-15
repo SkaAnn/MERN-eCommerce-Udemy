ce2f465f7d7dc73d81d132ec7f4d49febcf4ffb1
SkaAnn <xskachova@stuba.sk>
2021-02-15 13:52:39 +0100

    Get order by id

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -29,4 +29,22 @@ const addOrderItems = asyncHandler(async (req, res) => {
     }
 })
 
-export { addOrderItems }
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+    // ziskaj aj meno user-a ktoremu patri objednavka
+    const order = await Order.findById(req.params.id).populate(
+      'user',
+      'name email'
+    )
+  
+    if (order) {
+      res.json(order)
+    } else {
+      res.status(404)
+      throw new Error('Order not found')
+    }
+  })
+
+export { addOrderItems, getOrderById }
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { addOrderItems } from '../controllers/orderController.js'
+import { addOrderItems, getOrderById } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 // /api/orders je tam kde to volame
 router.route('/').post(protect, addOrderItems)
+router.route('/:id').get(protect, getOrderById)
 
 export default router
