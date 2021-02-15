088085f8707268bf3d6455d48aad7ca1b6fb4999
SkaAnn <xskachova@stuba.sk>
2021-02-15 09:49:44 +0100

    Shipping screen

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,8 +1,8 @@
 // only one reducer...
 
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstants'
 
-export const cartReducer = (state = { cartItems: [] }, action) => {
+export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, action) => {
     // cartItems - mozme mat viac poloziek v kosiku
     switch (action.type) {
         case CART_ADD_ITEM:
@@ -23,11 +23,15 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
                 }
             }
         case CART_REMOVE_ITEM:
-            return{
+            return {
                 ...state,
-                cartItems: state.cartItems.filter( x => x.product !== action.payload )
-            }    
-
+                cartItems: state.cartItems.filter(x => x.product !== action.payload)
+            }
+        case CART_SAVE_SHIPPING_ADDRESS:
+            return {
+                ...state,
+                shippingAddress: action.payload,
+            }
         default:
             return state
     }
