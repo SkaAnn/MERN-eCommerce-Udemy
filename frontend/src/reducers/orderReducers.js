12a63bb7556e6c1fa1a7509236bb7637b30bb5b6
SkaAnn <xskachova@stuba.sk>
2021-02-15 20:52:47 +0100

    Fix profile detail and order reset

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -2,7 +2,7 @@ import {
     ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL,
     ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL,
     ORDER_PAY_RESET, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL,
-    ORDER_LIST_MY_REQUEST, ORDER_LIST_MY_SUCCESS, ORDER_LIST_MY_FAIL
+    ORDER_LIST_MY_REQUEST, ORDER_LIST_MY_SUCCESS, ORDER_LIST_MY_FAIL, ORDER_LIST_MY_RESET
 } from '../constants/orderConstants'
 
 // order create reducer
@@ -89,6 +89,8 @@ export const orderListMyReducer = (state = { orders: [] }, action) => {
                 loading: false,
                 error: action.payload,
             }
+        case ORDER_LIST_MY_RESET:
+            return { orders: [] }
         default:
             return state
     }
