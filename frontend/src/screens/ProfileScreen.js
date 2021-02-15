e0793cadbc7e8154a8ffec86ee90f58597875a8c
SkaAnn <xskachova@stuba.sk>
2021-02-15 17:48:14 +0100

    List user orders

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Form, Button, Row, Col } from 'react-bootstrap'
+import { Form, Button, Row, Col, Table } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { getUserDetails, updateUserProfile } from '../actions/userActions'
+import { listMyOrders } from '../actions/orderActions'
 
 const ProfileScreen = ({ location, history }) => {
     // Component level state
@@ -25,13 +26,18 @@ const ProfileScreen = ({ location, history }) => {
     const userUpdateProfile = useSelector((state) => state.userUpdateProfile)
     const { success } = userUpdateProfile
 
+    // orderListMy state frome store.js
+    const orderListMy = useSelector(state => state.orderListMy)
+    const { loading: loadingOrders, error: errorOrders, orders } = orderListMy
+
     // redirect when we are sign in
     useEffect(() => {
         if (!userInfo) {
             history.push('/login')
         } else {
-            if (!user.name) {  
+            if (!user.name) {
                 dispatch(getUserDetails('profile')) // redirect to /profile
+                dispatch(listMyOrders())
             } else {
                 setName(user.name)
                 setEmail(user.email)
@@ -43,12 +49,12 @@ const ProfileScreen = ({ location, history }) => {
     const submitHandler = (e) => {
         e.preventDefault()
         if (password !== confirmPassword) {
-          setMessage('Passwords do not match')
+            setMessage('Passwords do not match')
         } else {
-          // DISPATCH UPDATE PROFILE
-          dispatch(updateUserProfile({ id: user._id, name, email, password }))
+            // DISPATCH UPDATE PROFILE
+            dispatch(updateUserProfile({ id: user._id, name, email, password }))
         }
-      }
+    }
 
     return (
         <Row>
@@ -86,6 +92,40 @@ const ProfileScreen = ({ location, history }) => {
             </Col>
             <Col md={9}>
                 <h2>My Orders</h2>
+                {loadingOrders ? <Loader /> :
+                    errorOrders ? <Message variant='danger'>{errorOrders}</Message>
+                        : (
+                            <Table striped bordered hover responsive className='table-sm'>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>DATE</th>
+                                        <th>TOTAL</th>
+                                        <th>PAID</th>
+                                        <th>DELIVERED</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {orders.map(order => (
+                                        <tr key={order._id}>
+                                            <td>{order._id}</td>
+                                            <td>{order.createdAt.substring(0, 10)}</td>
+                                            <td>{order.totalPrice}</td>
+                                            <td>{order.isPaid ? order.paidAt.substring(0, 10)
+                                                : (<i className='fas fa-times' style={{ color: 'red' }} />)}</td>
+                                            <td>{order.isDelivered ? order.deliveredAt.substring(0, 10)
+                                                : (<i className='fas fa-times' style={{ color: 'red' }} />)}</td>
+                                            <td>
+                                                <LinkContainer to={`/order/${order._id}`}>
+                                                    <Button className='btn-sm' variant='light'>Details</Button>
+                                                </LinkContainer>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table>
+                        )}
             </Col>
         </Row>
 
