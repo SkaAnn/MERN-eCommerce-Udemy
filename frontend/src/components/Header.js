4e0e6a668031d46bf906172b0b0d266d75da02bb
SkaAnn <xskachova@stuba.sk>
2021-02-16 10:41:39 +0100

    User list screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -34,12 +34,25 @@ const Header = () => {
                                         <NavDropdown.Item>Profile</NavDropdown.Item>
                                     </LinkContainer>
                                     <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
-                                </NavDropdown>) 
+                                </NavDropdown>)
                                 :
                                 (<LinkContainer to='/login'>
                                     <Nav.Link><i className='fas fa-user' /> Sign in</Nav.Link>
                                 </LinkContainer>)
                             }
+
+                            {userInfo && userInfo.isAdmin &&
+                                (<NavDropdown title='Admin' id='adminmenu'>
+                                    <LinkContainer to='/admin/userlist'>
+                                        <NavDropdown.Item>Users</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to='/admin/productlist'>
+                                        <NavDropdown.Item>Products</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to='/admin/orderlist'>
+                                        <NavDropdown.Item>Orders</NavDropdown.Item>
+                                    </LinkContainer>
+                                </NavDropdown>)}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -3,6 +3,10 @@ import {
     USER_DETAILS_REQUEST,
     USER_DETAILS_RESET,
     USER_DETAILS_SUCCESS,
+    USER_LIST_FAIL,
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_RESET,
     USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT,
     USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS
 } from "../constants/userConstants"
@@ -44,7 +48,7 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
         case USER_DETAILS_FAIL:
             return { loading: false, error: action.payload }
         case USER_DETAILS_RESET:
-            return { user: {}}
+            return { user: {} }
         default:
             return state
     }
@@ -58,6 +62,21 @@ export const userUpdateProfileReducer = (state = {}, action) => {
             return { loading: false, success: true, userInfo: action.payload }
         case USER_UPDATE_PROFILE_FAIL:
             return { loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
+
+export const userListReducer = (state = { users: [] }, action) => {
+    switch (action.type) {
+        case USER_LIST_REQUEST:
+            return { loading: true }
+        case USER_LIST_SUCCESS:
+            return { loading: false, users: action.payload }
+        case USER_LIST_FAIL:
+            return { loading: false, error: action.payload }
+        case USER_LIST_RESET:
+            return { users: []}
         default:
             return state
     }
