f7569f813cc3cdf4e1b16b1883ef02f511ab9377
SkaAnn <xskachova@stuba.sk>
2021-02-16 09:23:10 +0100

    Admin middleware and user list backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, registerUser, getUserProfile, updateUserProfile } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { authUser, registerUser, getUserProfile, updateUserProfile, getUsers } from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 // /api/users je tam kde to volame
-router.route('/').post(registerUser)
+router.route('/')
+    .post(registerUser)
+    .get(protect, admin, getUsers)  // middleware protect and admin
 router.post('/login', authUser)
 router
     .route('/profile')
