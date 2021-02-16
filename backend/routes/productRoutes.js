13b810bf2cda2e118041c86ef286e5ea53696040
SkaAnn <xskachova@stuba.sk>
2021-02-16 15:59:57 +0100

    Delete product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express'
 const router = express.Router()
-import { getProductById, getProducts } from '../controllers/productController.js'
+import { getProductById, getProducts, deleteProduct } from '../controllers/productController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 // /api/products je tam kde to volame
 router.route('/').get(getProducts)
-router.route('/:id').get(getProductById)
+router.route('/:id')
+    .get(getProductById)
+    .delete(protect, admin, deleteProduct)
 
 export default router
