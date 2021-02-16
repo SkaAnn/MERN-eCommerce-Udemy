8524242125aa3ed4a62df3594bae5a12afe48e12
SkaAnn <xskachova@stuba.sk>
2021-02-16 11:55:08 +0100

    Edit user backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,7 +36,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     const userExist = await User.findOne({ email })
 
-    if(userExist){
+    if (userExist) {
         res.status(400)
         throw new Error('User already exists')
     }
@@ -45,7 +45,7 @@ const registerUser = asyncHandler(async (req, res) => {
         name, email, password,
     })
 
-    if(user) {
+    if (user) {
         res.status(201).json({
             _id: user._id,
             name: user.name,
@@ -53,7 +53,7 @@ const registerUser = asyncHandler(async (req, res) => {
             isAdmin: user.isAdmin,
             token: generateToken(user._id),
         })
-    }else{
+    } else {
         res.status(400)
         throw new Error('Invalid user data')
     }
@@ -83,21 +83,21 @@ const getUserProfile = asyncHandler(async (req, res) => {
 const updateUserProfile = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id)
     if (user) {
-       user.name = req.body.name || user.name
-       user.email = req.body.email || user.email
-       if (req.body.password){
-           user.password = req.body.password
-       }
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if (req.body.password) {
+            user.password = req.body.password
+        }
 
-       const updatedUser = await user.save()
+        const updatedUser = await user.save()
 
-       res.json({
-        _id: updatedUser._id,
-        name: updatedUser.name,
-        email: updatedUser.email,
-        isAdmin: updatedUser.isAdmin,
-        token: generateToken(updatedUser._id),
-    })
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id),
+        })
 
     } else {
         res.status(404)
@@ -110,7 +110,7 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 // @access  Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
     const users = await User.find({})
-    res.json(users)  
+    res.json(users)
 })
 
 // @desc    Delete user
@@ -118,13 +118,54 @@ const getUsers = asyncHandler(async (req, res) => {
 // @access  Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id)
-    if (user){
+    if (user) {
         await user.remove()
-        res.json({ message: 'User removed'})
-    }else{
+        res.json({ message: 'User removed' })
+    } else {
         res.status(404)
         throw new Error('User not found')
     }
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser }
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id).select('-password')
+    if (user) {
+        res.json(user)
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id)
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        user.isAdmin = req.body.isAdmin || user.isAdmin // bez tej druhej casti to neslo!
+
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        })
+
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+export {
+    authUser, registerUser, getUserProfile, updateUserProfile,
+    getUsers, deleteUser, getUserById, updateUser
+}
