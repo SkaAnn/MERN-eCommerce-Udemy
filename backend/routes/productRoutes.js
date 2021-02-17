8a5a0ad8eb1fa85871ca4edde2f268247391c379
SkaAnn <xskachova@stuba.sk>
2021-02-17 09:17:28 +0100

    Create and update product backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express'
 const router = express.Router()
-import { getProductById, getProducts, deleteProduct } from '../controllers/productController.js'
+import { getProductById, getProducts, deleteProduct, updateProduct, createProduct } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 // /api/products je tam kde to volame
-router.route('/').get(getProducts)
+router.route('/')
+    .get(getProducts)
+    .post(protect, admin, createProduct)
 router.route('/:id')
     .get(getProductById)
     .delete(protect, admin, deleteProduct)
+    .put(protect, admin, updateProduct)
 
 export default router
