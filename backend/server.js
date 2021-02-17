a750c3405afb0c4fbeb362f62b43077f4f8fd61f
SkaAnn <xskachova@stuba.sk>
2021-02-17 11:44:53 +0100

    Image upload backend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -7,6 +8,7 @@ import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -23,8 +25,14 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+// make static folder /uploads, aby nebol pristupny zvonku
+const __dirname = path.resolve()    //__dirname nemozno pouzivat s Module interpretaciou, iba s ESx
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))   
+
 
 // Not routing sites
 app.use(notFound)
